7cf77e325633c4b7f32d7c1ba69d44d23adab595
xorrior <christopher.ross@zoom.us>
2021-09-01 17:01:10 -0400

    Modified the function signature for the persist_loginitem to accept and return an interger

--- Payload_Type/poseidon/agent_code/persist_loginitem/persist_loginitem_darwin.h
+++ Payload_Type/poseidon/agent_code/persist_loginitem/persist_loginitem_darwin.h
@@ -9,6 +9,6 @@
 #ifndef persist_loginitem_h
 #define persist_loginitem_h
 
-extern bool persist_loginitem(char *path, char *name, bool global);
+extern int persist_loginitem(char* path, char* name, int global);
 
 #endif /* persist_loginitem_h */
