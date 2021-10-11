1932aaade93abf930b5afc84a270254da4e02b44
xorrior <christopher.ross@zoom.us>
2021-10-11 03:17:29 -0400

    Fix runCommand parameter

--- Payload_Type/poseidon/agent_code/dyld_inject/dyld_inject_linux.go
+++ Payload_Type/poseidon/agent_code/dyld_inject/dyld_inject_linux.go
@@ -14,7 +14,7 @@ func (j *DyldInjectLinux) Success() bool {
 	return j.Successful
 }
 
-func runCommand(app string, dylib string, hide int) (DyldInjectLinux, error) {
+func runCommand(app string, dylib string, hide bool) (DyldInjectLinux, error) {
 	n := DyldInjectLinux{}
 	n.Successful = false
 	return n, errors.New("Not implemented")
