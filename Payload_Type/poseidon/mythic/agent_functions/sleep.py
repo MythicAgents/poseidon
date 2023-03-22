5612ceeffc402a09750b7a3f4ea4182d01fb4891
Taylor Sano <taylor.sano@zoom.us>
2023-03-22 15:08:15 -0700

    add mythicRPC dep

--- Payload_Type/poseidon/mythic/agent_functions/sleep.py
+++ Payload_Type/poseidon/mythic/agent_functions/sleep.py
@@ -1,6 +1,6 @@
 from mythic_payloadtype_container.MythicCommandBase import *
 import json
-
+from mythic_payloadtype_container.MythicRPC import *
 
 class SleepArguments(TaskArguments):
     def __init__(self, command_line, **kwargs):
